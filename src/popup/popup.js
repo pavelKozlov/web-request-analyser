5d58c90a1c78ae2151ad18ee6c135090727df75b
Pavel Kozlov <pavelkozlov@Pavels-MacBook-Pro-2.local>
2019-11-01 15:59:25 +0100

    add enabling/disabling domains

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -9,19 +9,47 @@ document.getElementById('clear-button').addEventListener('click', () => {
   refresh();
 });
 
-console.log('popup.js');
+document.getElementById('content-list').addEventListener('click', (event) => {
+  const domain = event.target.querySelector('.domain-text').textContent;
+  chrome.storage.local.get({
+    disabled: {}
+  },
+  ({disabled}) => {
+    let newDisabled = {
+      ...disabled
+    };
+    let shouldDisabled = !disabled[domain];
+    if (shouldDisabled) {
+      newDisabled[domain] = true;
+    } else {
+      delete newDisabled[domain];
+    }
+
+    chrome.storage.local.set({
+      disabled: newDisabled
+    },
+    () => {
+      updateItem(domain, shouldDisabled);
+    } 
+  )
+  })
+});
+
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOMContentLoaded');
   refresh();
 });
 
 function refresh() {
   chrome.storage.local.get({
       urlList: [],
+      disabled: {}
     },
     (data) => {
       console.log('getDate callback:', data);
-      addUrlsToTheDOM(groupData(data.urlList));
+      addUrlsToTheDOM(
+        groupData(data.urlList),
+        data.disabled
+      );
     } 
   )
 }
@@ -41,16 +69,41 @@ function groupData(urlList) {
   }));
 }
 
-function addUrlsToTheDOM(urlList) {
+function addUrlsToTheDOM(urlList, disabled) {
   const list = document.getElementById('content-list');
   list.innerHTML = '';
   for (let data of urlList) {
-    list.appendChild(getUrlElement(data));
+    const isDisabled = disabled[data.domain];
+    list.appendChild(getUrlElement(data, isDisabled));
   };
 }
 
-function getUrlElement({domain, count}) {
+function getUrlElement({domain, count}, isDisabled) {
   const listItem = document.createElement('li');
-  listItem.textContent = domain + '(' + count + ')';
+  if (isDisabled) {
+    listItem.classList.add('disabled');
+  }
+  const domainText = document.createElement('span');
+  domainText.textContent = domain;
+  domainText.className = 'domain-text';
+  listItem.appendChild(domainText);
+
+  const countText = document.createElement('span');
+  countText.textContent = '(' + count + ')';
+  listItem.appendChild(countText);
+
   return listItem;
+}
+
+function updateItem(text, isDisabled) {
+  const list = document.querySelectorAll('#content-list > li');
+  for (let domainElement of list) {
+    if (domainElement.querySelector('.domain-text').textContent === text) {
+      const method = isDisabled ? 'add' : 'remove';
+      domainElement.classList[method]('disabled');
+      console.log('found!');
+      break;
+    }
+  }
+
 }
